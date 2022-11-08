fb7bb204406eee837ad5f635cf1e585cb2ce7761
alexmealster19 <96315943+alexmealster19@users.noreply.github.com>
2022-11-08 15:49:28 -0500

    Update tmp.sh

--- tmp.sh
+++ tmp.sh
@@ -2,3 +2,6 @@ sudo apt remove libboost-thread1.71.0 libboost-thread1.71-dev libboost-serializa
 gnuradio gnuradio-dev gr-fosphor gr-osmosdr libboost-serialization1.71.0
   libboost-thread1.71.0 libgnuradio-fosphor3.8.0 libgnuradio-osmosdr0.2.0
   libgnuradio-uhd3.8.2 libuhd-dev libuhd3.15.0 uhd-host
+
+
+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
