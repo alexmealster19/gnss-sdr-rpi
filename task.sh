127651faed54b18600efdf0a12faf94f187b708d
alexmealster19 <96315943+alexmealster19@users.noreply.github.com>
2022-12-01 16:50:13 -0500

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+apt-get update
+apt install hackrf
+apt-get update
+apt-get update --allow-releaseinfo-change
 apt-get install build-essential cmake git pkg-config libboost-dev libboost-date-time-dev \
        libboost-system-dev libboost-filesystem-dev libboost-thread-dev libboost-chrono-dev \
        libboost-serialization-dev liblog4cpp5-dev libuhd-dev gnuradio-dev gr-osmosdr \
        libblas-dev liblapack-dev libarmadillo-dev libgflags-dev libgoogle-glog-dev \
        libgnutls-openssl-dev libpcap-dev libmatio-dev libpugixml-dev libgtest-dev \
        libprotobuf-dev protobuf-compiler python3-mako
+git clone https://github.com/gnss-sdr/gnss-sdr
+cd gnss-sdr/build
+cmake -DENABLE_OSMOSDR=ON ../
+make
+sudo make install
