82b21910bc57bbee0c7cd75864fbf7813810cc0c
alexmealster19 <96315943+alexmealster19@users.noreply.github.com>
2023-02-20 15:53:58 -0500

    Update and rename longCommand to longCommand.sh

--- build/longCommand.sh
+++ build/longCommand.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 apt-get install build-essential cmake git pkg-config libboost-dev libboost-date-time-dev \
        libboost-system-dev libboost-filesystem-dev libboost-thread-dev libboost-chrono-dev \
        libboost-serialization-dev liblog4cpp5-dev libuhd-dev gnuradio-dev gr-osmosdr \
